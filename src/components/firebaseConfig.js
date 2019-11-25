fbda26253ea24b108997e17030dfe880a4c78666
murrey <stanleymurrey08@gmail.com>
2019-11-25 16:45:44 +0000

    first commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default{apiKey: "AIzaSyD22xlhUXjfOzryg0VWrwPvr5pb7k7_2bc",
+authDomain: "vuefs-prod-3fc1c.firebaseapp.com",
+databaseURL: "https://vuefs-prod-3fc1c.firebaseio.com",
+projectId: "vuefs-prod-3fc1c",
+storageBucket: "vuefs-prod-3fc1c.appspot.com",
+messagingSenderId: "590819366600",
+appId: "1:590819366600:web:8e6d82253502c1b47e37fd",
+measurementId: "G-DXTP26PTWV"
+}
